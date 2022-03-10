4989715d4eb51eb73977ddeafd6ad26cc162a0e6
JoshMeni <MY_NAME@example.com>
2022-03-10 11:04:24 -0700

    another

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const challengeTable = document.querySelector('challenge-table')
 challengeTable.data = [
   [1, 1],
   [2, 2],
+  [3, 3],
 ];
+
 challengeTable.tableName = 'Linear Data'
 
 document.querySelector('#smallButton').addEventListener('click', async () => {
@@ -24,28 +26,9 @@ document.querySelector('#mediumButton').addEventListener('click', async () => {
 
 document.querySelector('#largeButton').addEventListener('click', async () => {
   // code for event handler
-  dataSet={ 
-    name: "small",
-    xColumn: {
-      name: 'x',
-      values: [1,2,3,4,5,6,7]
-    },
-    yColumn: {
-      name: 'y',
-      values: [1,2,3,4,5,6,7]
-    }
-  }
-
-  data = [
-    [x1, y1],
-    [x2, y2],
-    [x3, y3],
-  ]
-  document.querySelector('challenge-table').small = data;
-  const data = dataSet.xColumn.values.map((value, index) => [value, dataSet.yColumn.values[index]]);
+  var xValues = [50,60,70,80,90,100,110,120,130,140,150];
+  var yValues = [7,8,8,9,9,9,10,11,14,14,15];
+  // const data = dataSet.xColumn.values.map((value, index) => [value, dataSet.yColumn.values[index]]);
   const data = dataSet.xColumn.values.map((value, index) => ({x: value, y: dataSet.yColumn.values[index]}));
 });
 
-
-
-  
